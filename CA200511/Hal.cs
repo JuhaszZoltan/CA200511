34d80b55e24a95f0f166b23a755f68b574ad67d5
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-05-11 10:26:46 +0200

    asd

---
+++ CA200511/Hal.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200511
+{
+    class Hal
+    {
+        public static string[] Fajtak = { "ponty", "harcsa", "busa", "kárasz", "bohóchal", "aranyhal", "keszeg", "polip" };
+
+        public float Suly
+        { 
+            get { return _suly; }
+            set
+            {
+                if (_suly != 0 &&  value > _suly * 1.1F) 
+                    throw new Exception("ennyive egyszerre nem változhat a súlya!");
+                if (value < .5F) throw new Exception("túl alacsony a súlya!");
+                if (value > 40) throw new Exception("túl magag a súlya!");
+                _suly = value;
+            }
+        }
+        private float _suly;
+
+        public bool Ragadozo 
+        { 
+            get { return _ragadozo; }
+            set
+            {
+                if (_isRagadozoSet) throw new Exception("nem változtatható meg!");
+                _isRagadozoSet = true;
+                _ragadozo = value;
+            }
+        }
+        private bool _ragadozo;
+        private bool _isRagadozoSet = false;
+
+        public int MinMelyseg
+        {
+            get { return _minMelyseg; }
+            set
+            {
+                if (value < 0) throw new Exception("nem léteznek repülő halak");
+                if (value > 400) throw new Exception("nem lehet ilyen mély a minimum");
+                _minMelyseg = value;
+            }
+        }
+        private int _minMelyseg;
+
+        public int Merules 
+        {
+            get { return _merules; } 
+            set
+            {
+                if (value < 10) throw new Exception("nem lehet ilyen szűk a merülési sáv!");
+                if (value > 400) throw new Exception("nem lehet ilyen széles a merülési sáv!");
+                _merules = value;
+            }
+        }
+        private int _merules;
+        public string Faj
+        {
+            get { return _faj; }
+            set
+            {
+                if (value is null) throw new Exception("nem lehet null!");
+                if (value.Length < 3) throw new Exception("túl rövig faj-azonosító!");
+                if (value.Length > 30) throw new Exception("túl hosszú faj-azonosító!");
+                _faj = value;
+            }
+        }
+        private string _faj;
+
+
+    }
+}
--- CA200511/Program.cs
+++ CA200511/Program.cs
@@ -8,8 +8,44 @@ namespace CA200511
 {
     class Program
     {
+        static Random rnd = new Random();
+        static List<Hal> halak;
         static void Main(string[] args)
         {
+            Feltolt(100);
+            OsszesKiir();
+
+            Console.ReadKey();
+        }
+
+        private static void OsszesKiir()
+        {
+            foreach (var h in halak)
+            {
+                Console.WriteLine("{0, -9} {1, 10} {2, 5} Kg él: [{3} - {4}]",
+                    h.Ragadozo ? "ragadozó" : "növényevő",
+                    h.Faj, 
+                    h.Suly,
+                    h.MinMelyseg, 
+                    h.MinMelyseg + h.Merules);
+            }
+        }
+
+        private static void Feltolt(int db)
+        {
+            halak = new List<Hal>();
+
+            for (int i = 0; i < db; i++)
+            {
+                halak.Add(new Hal()
+                {
+                    Ragadozo = rnd.Next(100) >= 90,
+                    Suly = rnd.Next(5, 400) / 10F,
+                    MinMelyseg = rnd.Next(400),
+                    Merules = rnd.Next(10, 400),
+                    Faj = Hal.Fajtak[rnd.Next(Hal.Fajtak.Length)],
+                });
+            }
         }
     }
 }
